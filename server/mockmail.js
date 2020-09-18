930b015943a9057e826565fe7ade82f5d93e9ff4
Sathyababuram <ksathyababuram95@gmail.com>
2020-09-18 21:24:37 +0530

    File changes

--- server/mockmail.js
+++ server/mockmail.js
@@ -7,23 +7,11 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-// app.get("/*", function(req, res) {
-//     res.sendFile(path.join(__dirname, "index.html"));
-//   });
+app.use(express.static(__dirname + '../client/public'));
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// app.use('/', (err,res)=>{
-
-// });
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-// const err = new Error('Not Found');
-// err.status = 404;
-// next(err);
-// });
 
 const port = process.env.port || 3030;
 app.listen(port, function(req, res) {
